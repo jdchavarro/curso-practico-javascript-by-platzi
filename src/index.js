ac9a35daee31b65910cafe6a79d8ad6546d5fbfb
jdchavarro <jd-chavarro@hotmail.com>
2022-10-07 21:55:53 -0500

    desarrollando descuentos

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import '@styles/index.css';
 import '@styles/tablet.css';
 import '@styles/laptop.css';
 import '@styles/desktop.css';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
+import './modules/descuentos';
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js', './src/modules/descuentos.js'],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
