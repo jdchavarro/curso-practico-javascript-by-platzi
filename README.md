b39309a1141131819202a0711aca1f89b94700bd
jdchavarro <jdchavarro@DESKTOP-RD6GTQ9>
2023-02-25 18:59:23 -0500

    docs: cambios en el nombre del repositorio, ajustes en el readme

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
-# platzi-curso-practico-javascript
+# Proyecto Figuras Geometricas, Medidas de Tendencia y Calculadora Salario
+
+Es un proyecto basado en el curso practico de javascript de platzi, en el que se nos plantea unos ejercicios practicos, aqui estarian todos los ejercicios terminados de dicha practica, agregando mi toque personal
+
+Una vez descargado el proyecto:
+`npm install`
+
+Proyecto aun en desarrollo, para ver avances ejecutar
+`npm run start`
+
+La ultima ejecucion completa puede encontrarse en la ruta
+`/dist/index.html`
 
 ...
 
-## Taller #1: Figuras Geometricas
+El proyecto consta de 6 mini programas
 
-- Primer: definir las formulas
-- Segundo: implementar la formulas en javascript
-- Tercero: crear funciones
-- Cuarto: integrar JS con HTML
-
-## Taller #2: Porcentajes y Descuentos
-
-- Primer: definir las formulas
-- Segundo: implementar la formulas en javascript
-- Tercero: crear funciones
-- Cuarto: integrar JS con HTML
-
-## Taller #3: Promedia, mediana y moda
-
-- Primer: definir las formulas
-- Segundo: implementar la formulas en javascript
-- Tercero: crear funciones
-- Cuarto: integrar JS con HTML
+1. Calcula el descuento en productos
+2. Calcula el area y promedio de figuras geometricas (cuadrado, circulo y triangulo)
+3. Calcula la mediada de numeros dados
+4. Calcula la moda de numeros dados
+5. Calcula el promedio de numeros dados
+6. Calcula el salario
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,15 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    descuentos: './src/modules/descuentos.js',
+    figuras: './src/modules/figuras.js',
+    mediana: './src/modules/mediana.js',
+    moda: './src/modules/moda.js',
+    promedio: './src/modules/promedio.js',
+    salario: './src/modules/salario.js',
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
@@ -51,6 +59,42 @@ module.exports = {
       inject: true,
       template: "./public/index.html",
       filename: "./index.html"
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/actividades/descuentos.html",
+      filename: "./actividades/descuentos.html",
+      chunks: ['index','descuentos']
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/actividades/figuras.html",
+      filename: "./actividades/figuras.html",
+      chunks: ['index','figuras']
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/actividades/mediana.html",
+      filename: "./actividades/mediana.html",
+      chunks: ['index','mediana']
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/actividades/moda.html",
+      filename: "./actividades/moda.html",
+      chunks: ['index','moda']
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/actividades/promedio.html",
+      filename: "./actividades/promedio.html",
+      chunks: ['index','promedio']
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/actividades/salario.html",
+      filename: "./actividades/salario.html",
+      chunks: ['index','salario']
     }),
     new MiniCssExtractPlugin({
       filename: "styles/[name].[contenthash].css"
